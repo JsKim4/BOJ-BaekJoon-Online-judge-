b817d561eeee689666f22f792936b18932e9fb08
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-12 20:15:59 +0900

    nc

---
+++ java/S11000/S11600/P11656.java
@@ -0,0 +1,22 @@
+package S11000.S11600;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P11656 {
+	public static void main(String[] args) {
+		String a;
+		String s[];
+		Scanner sc = new Scanner(System.in);
+		a=sc.next();
+		s=new String[a.length()];
+		for(int i=0;i<a.length();i++) {
+			s[i]=a.substring(i,a.length());
+		}
+		
+		Arrays.sort(s);
+		for(int i=0;i<a.length();i++) {
+			System.out.println(s[i]);
+		}
+	}
+}
---
+++ java/S2000/S2100/P2110.java
@@ -0,0 +1,7 @@
+package S2000.S2100;
+
+public class P2110 {
+	public static void main(String args[]) {
+		
+	}
+}
