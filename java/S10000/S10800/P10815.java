c1ff8f26db8e3459fea53cb801afb58e91b77650
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-14 15:24:23 +0900

    숫자카드

---
+++ java/S10000/S10800/P10815.java
@@ -0,0 +1,60 @@
+package S10000.S10800;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+// 10815 숫자카드
+public class P10815 {
+	int nArray[];
+	public boolean search(int num, int r) {
+		int left = 0;
+		int right = r;
+		if(nArray[r]==num)
+			return true;
+		while(true) {
+			int mid = (left+right)/2;
+			if(nArray[mid]==num) {
+				return true;
+			}
+
+			if(left==mid)
+				return false;
+			if(nArray[mid]>num) {
+				right = mid;
+			}else {
+				left = mid;
+			}
+		}
+	}
+
+	public void solve() throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(in.readLine());
+		String nA[] = in.readLine().split(" ");
+		nArray = new int[n];
+		for (int i = 0; i < n; i++) {
+			nArray[i] = Integer.parseInt(nA[i]);
+		}
+		Arrays.sort(nArray);
+		int m = Integer.parseInt(in.readLine());
+		String mA[] = in.readLine().split(" ");
+		for (int i = 0; i < m; i++) {
+			if(search(Integer.parseInt(mA[i]),n-1)) {
+				System.out.print("1 ");
+			}
+			else {
+				System.out.print("0 ");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			new P10815().solve();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
