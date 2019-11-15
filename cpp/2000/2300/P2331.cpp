a890d84149b340c36746d76c98e6c2a01f6245a6
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-15 19:16:59 +0900

    반복수열

---
+++ cpp/2000/2300/P2331.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<cmath>
+#pragma warning(disable:4996)
+using namespace std;
+int dp[300000];
+int cal(int n,int p){
+	int result = 0;
+	result += pow(n % 100000 / 10000, p);
+	result += pow(n % 10000 / 1000, p);
+	result += pow(n % 1000 / 100, p);
+	result += pow(n % 100 / 10, p);
+	result += pow(n % 10, p);
+	return result;
+}
+int main() {
+	int n, p;
+	scanf("%d %d",&n,&p);
+	int count = 1;
+	dp[n] = 1;
+	while (true) {
+		n = cal(n, p);
+		if (dp[n])
+			break;
+		count++;
+		dp[n] = count;
+	}
+	printf("%d",dp[n]-1);
+}
