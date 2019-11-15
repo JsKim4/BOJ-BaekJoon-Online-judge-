04f728b6290492a3b52f8fef5c808a18aaea11d6
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-15 20:38:07 +0900

    텀 프로젝트
    알고리즘 자체는 금방 구현 했으나 시간복잡도 때문에 고생했음
    처음에는 passCount를 반복 1회마다 resize해주어 시간초과가 발생하였음
    passLoad로 어떤 학생을 거쳤는지 저장하고 passCount를 passLoad에 있는 학생만 초기화함

---
+++ cpp/9000/9400/P9466.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#pragma warning (disable:4996)
+using namespace std;
+int list[100001];
+vector<int> passCount;
+vector<int> passLoad;
+// 10451
+int check(int nowN,int count) {
+	//printf("%d %d\n",nowN,count);
+	passLoad.push_back(nowN);
+	if (passCount[nowN] > 0) {
+		return count +1 - passCount[nowN];
+	}
+	if (list[nowN] == 0&&passCount[nowN]<=0)
+		return 0;
+	int num = list[nowN];
+	passCount[nowN] = count + 1;
+	list[nowN] = 0;
+	return check(num,count+1);
+}
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	while (T--) {
+		int n;
+		scanf("%d",&n);
+		int count = 0;
+		for (int i = 1; i <= n; i++) {
+			scanf("%d", &list[i]);
+			if (i == list[i]) {
+				count++;
+				list[i] = 0;
+			}
+		}
+		passCount.resize(n + 1, 0);
+		for (int i = 1; i <= n; i++) {
+			if (list[i] == 0)
+				continue;
+			count += check(i,0);
+			for (int i = 0; i < passLoad.size();i++) {
+				passCount[passLoad[i]] = 0;
+			}
+			passLoad.clear();
+		}
+		printf("%d\n", n-count);
+	}
+
+} 
