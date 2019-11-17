a4e22af3dbfb859a3b12d50ea7803f1754386316
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-17 20:00:33 +0900

    트리의 지름

--- cpp/1000/1100/P1167.cpp
+++ cpp/1000/1100/P1167.cpp
@@ -4,30 +4,19 @@
 #pragma warning (disable:4996)
 using namespace std;
 // 1167
-vector<pair<int,int>>node[100001];
+vector<pair<int, int>>node[100001];
 int n;
 int length[100001];
 bool parent[100001];
-int lenBfs(int i) {
-	int bm = 0;
-	int am = 0;
-	if (parent[i]==false&&node[i].size()==1) {
-		//printf("node last %d\n",i);
-		return 0;
-	}
+void lenBfs(int i, int num) {
 	parent[i] = true;
-	for (int j = 0; j < node[i].size();j++) {
-		if (parent[node[i][j].first] == true)
+	length[i] = num;
+	for (int j = 0; j < node[i].size(); j++) {
+		if (parent[node[i][j].first])
 			continue;
-		int l = lenBfs(node[i][j].first);
-		if (am<l+node[i][j].second) {
-			bm = am;
-			am = l + node[i][j].second;
-		}
+		//	printf("%d -> %d\n",i,node[i][j].first);
+		lenBfs(node[i][j].first, num + node[i][j].second);
 	}
-	length[i] = am+bm;
-	//printf("%d length %d \n",i,length[i]);
-	return am;
 }
 
 
@@ -35,24 +24,37 @@ int main() {
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		int a;
-		scanf("%d",&a);
+		scanf("%d", &a);
 		while (true) {
-			int b,c;
-			scanf("%d",&b);
+			int b, c;
+			scanf("%d", &b);
 			if (b == -1)
 				break;
-			scanf("%d",&c);
-			node[a].push_back(make_pair(b,c));
+			scanf("%d", &c);
+			node[a].push_back(make_pair(b, c));
 		}
 	}
-	parent[1] = true;
-	lenBfs(1);
 	int max = 0;
+	int mi = 1;
+	lenBfs(1, 0);
 	for (int i = 1; i <= n; i++) {
-		printf("%d ",length[i]);
-		if (length[i] > max)
+		parent[i] = false;
+		//	printf("%d length %d \n",i,length[i]);
+		if (max < length[i]) {
+			mi = i;
 			max = length[i];
+		}
+	}
+	//printf("mi %d\n",mi);
+	lenBfs(mi, 0);
+	max = 0;
+	mi = 1;
+	for (int i = 1; i <= n; i++) {
+		//		printf("%d length %d \n", i, length[i]);
+		if (max < length[i]) {
+			mi = i;
+			max = length[i];
+		}
 	}
 	printf("%d\n", max);
-	
 }
