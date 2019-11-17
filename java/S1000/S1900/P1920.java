7bb3b29c54df95c2e2969585c18f1c1513c561d8
kjs <46150254+JsKim4@users.noreply.github.com>
2019-11-17 16:53:07 +0900

    수 찾기

---
+++ java/S1000/S1900/P1920.java
@@ -0,0 +1,32 @@
+package S1000.S1900;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+// 1920 수 찾기
+public class P1920 {
+
+	public void solve() {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n = sc.nextInt();
+		Map<Integer, Boolean> m = new HashMap<>();
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			m.put(a, true);
+		}
+		n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			if(m.get(a)!=null) 
+				System.out.println(1);
+			else
+				System.out.println(0);
+		}
+
+	}
+
+	public static void main(String[] args) {
+		new P1920().solve();
+	}
+}
