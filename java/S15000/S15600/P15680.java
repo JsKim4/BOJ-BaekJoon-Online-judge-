79136e5f707722f9116649f6d3150e11540e92d4
ParkHoGwan <ggi4111@naver.com>
2020-01-14 11:43:12 +0900

    연세대학교

---
+++ java/S15000/S15600/P15680.java
@@ -0,0 +1,17 @@
+package S15000.S15600;
+
+import java.util.Scanner;
+	
+// 15680 연세대학교
+public class P15680 {
+	public void solve() {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		System.out.println(n==0?"YONSEI":"Leading the Way to the Future");
+		
+	}
+	public static void main(String args[]) {
+		new P15680().solve();
+	}
+}
