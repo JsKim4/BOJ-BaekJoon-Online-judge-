fcfa00bd4fff91232de00e0a89643bf8fcf1e833
kjs <46150254+JsKim4@users.noreply.github.com>
2020-01-29 21:41:58 +0900

    치킨 배달

---
+++ cpp/15000/15600/P15686.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#pragma warning (disable:4996)
+using namespace std;
+// 15686 치킨 배달
+vector<pair<int, int>> house;
+int city[50][50];
+int min = 210000000;
+int main() {
+
+}
