9dc064ede47580d8467fa32998b3531c700c10fa
kjs <46150254+JsKim4@users.noreply.github.com>
2020-03-06 21:14:05 +0900

    금민수의 개수

--- cpp/1000/1500/P1527.cpp
+++ cpp/1000/1500/P1527.cpp
@@ -1,66 +1,28 @@
 #include<iostream>
 #include<cmath>
+#include<vector>
 #pragma warning (disable:4996)
 using namespace std;
 // 1527 금민수의 개수
-int ni[11];
-int mi[11];
-
-int wt(int n) {
-	int i = 1;
-	for (; (int)pow(10, i) <= n; i++);
-	return i-1;
+vector<int> s;
+int n, m;
+void gms(int i,int j,int t) {
+	if (t == 9)
+		return;
+	int cnt = j;
+	for (int ii = i; ii < j; ii++,cnt++)
+		s.push_back((int)pow(10, t) * 4 + s[ii]);
+	for (int ii = i; ii < j; ii++, cnt++)
+		s.push_back((int)pow(10, t) * 7 + s[ii]);
+	gms(j, cnt,t+1);
 }
 int main() {
+	s.push_back(4); s.push_back(7);
+	gms(0, 2, 1);
 	int n, m;
-	for (int i = 0; i < 11; i++) {
-		ni[i] = -1;
-		mi[i] = -1;
-	}
 	cin >> n >> m;
-
-	for (int i = wt(n); i >= 0; i--) {
-		int num = (n % (int)pow(10, i + 1)) / (int)pow(10, i);
-		if (num > 7) {
-			ni[i + 1]++;
-			ni[i] = 0;
-		}
-		else if (num > 4)
-			ni[i] = 1;
-		else
-			ni[i] = 0;
-	}
-	for (int i = wt(m); i >= 0; i--) {
-		int num = (m % (int)pow(10, i + 1)) / (int)pow(10, i);
-		if (num > 7) {
-			mi[i + 1]++;
-			mi[i] = 0;
-		}
-		else if (num > 4)
-			mi[i] = 1;
-		else
-			mi[i] = 0;
-	}
-	int nn = 0;
-	for (; ni[nn] != -1; nn++) {
-		if (ni[nn] == 2) {
-			ni[nn] = 0;
-			ni[nn + 1]++;
-		}
-	}
-	int mm = 0;
-	for (; mi[mm] != -1; mm++) {
-		if (ni[mm] == 2) {
-			ni[mm] = 0;
-			ni[mm + 1]++;
-		}
-	}
-	for (int i = 0; i < nn; i++) {
-		cout << ni[i];
-	}
-	cout << endl;
-	for (int i = 0; i < mm; i++) {
-		cout << ni[i];
-	}
-
+	int i,j;
+	for (i = 0; i<s.size() && s[i] < n; i++);
+	for (j = s.size() - 1; j >= 0 && s[j] > m; j--);
+	cout << j-i+1;
 }
