3646c22f92301698cb56d0a456a3973b5d74d7be
kjs <46150254+JsKim4@users.noreply.github.com>
2020-03-18 18:00:03 +0900

    소수의 곱 solving

---
+++ cpp/2000/2000/P2014.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#define INF 2147483648
+#pragma warning (disable:4996)
+using namespace std;
+// 2014 소수의 곱
+int main(){
+	int k, n;
+	int sq[100];
+	vector<int>ssq;
+	priority_queue<int>tq;
+	priority_queue<int>answer;
+	cin >>k>> n;
+	for (int i = 0; i < k; i++)
+		cin >> sq[i];
+}
