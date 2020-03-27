b62d39ed0ddfcfdfbbe77e355245b30282844e06
kjs <46150254+JsKim4@users.noreply.github.com>
2020-03-27 11:39:08 +0900

    소수의 곱

--- cpp/2000/2000/P2014.cpp
+++ cpp/2000/2000/P2014.cpp
@@ -1,17 +1,45 @@
 #include<iostream>
-#include<vector>
 #include<queue>
 #define INF 2147483648
 #pragma warning (disable:4996)
 using namespace std;
 // 2014 소수의 곱
-int main(){
+int sq[100];
+int len[100];
+int lena[100][100];
+int main() {
 	int k, n;
-	int sq[100];
-	vector<int>ssq;
-	priority_queue<int>tq;
-	priority_queue<int>answer;
-	cin >>k>> n;
-	for (int i = 0; i < k; i++)
+	priority_queue<long long>answer;
+	queue<pair<long long, int>>tq;
+	cin >> k >> n;
+	for (int i = 0; i < k; i++) {
 		cin >> sq[i];
+		tq.push({ sq[i],i });
+		answer.push(sq[i]);
+		len[i] = 1;
+	}
+	while (!tq.empty()) {
+		long long tnum = tq.front().first;
+		int j = tq.front().second; tq.pop();
+		for (; j < k; j++) {
+			long long nn = tnum * sq[j];
+			if (nn < INF) {
+				if (answer.size() <= n) {
+					answer.push(nn);
+					tq.push({ nn,j });
+					//cout << nn << endl;
+				}
+				else {
+					if (nn < answer.top()) {
+						answer.pop();
+						answer.push(nn);
+						tq.push({ nn,j });
+						//cout << nn << endl;
+					}
+				}
+			}
+		}
+	}
+	while (answer.size() > n)answer.pop();
+	cout << answer.top();
 }
