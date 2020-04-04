6b5fbe4e59e673e52017568fea9dcd3b6b8f8abd
kjs <46150254+JsKim4@users.noreply.github.com>
2020-04-04 12:09:01 +0900

    숨박꼭질5

--- cpp/17000/17000/P17071.cpp
+++ cpp/17000/17000/P17071.cpp
@@ -1,45 +1,60 @@
 #include<iostream>
 #include<queue>
 #include<vector>
+#include<map>
 #pragma warning (disable:4996)
 using namespace std;
 // 17071 숨박꼭질5
-int line[1001][500001];
+int line[500100][2];
 bool avail(int i) {
 	if (i >= 0 && i <= 500000)
 		return true;
 	return false;
 }
 int main(){
+	for (int i = 0; i < 500001;i++) {
+		line[i][0] = -1;
+		line[i][1] = -1;
+	}
 	int n, k;
 	cin >> n >> k;
 	int j = 0;
-	for (; k <= 500000; k += j) {
-		line[j][k] = j;
-		queue<int>q;
-		if (avail(j + 1))
-			q.push(j + 1);
-		if (avail(j - 1))
-			q.push(j - 1);
-		if (j != 0 && j % 2 == 0) 
-			q.push(j / 2);
-		for (int cnt = 0; cnt < j; cnt++) {
-			while (!q.empty()) {
-				int num = q.front(); q.pop();
-				if (avail(num + 1) && line[j - cnt][num + 1] != 0) {
-					line[j - cnt][num + 1] = k;
-					q.push(num + 1);
-				}
-				if (avail(num - 1) && line[j - cnt][num - 1] != 0) {
-					line[j - cnt][num - 1] = k;
-					q.push(num - 1);
-				}
-				if ((num != 0 && num % 2 == 0) && line[j - cnt][num / 2] != 0) {
-					line[j - cnt][num /2] = k;
-					q.push(num / 2);
-				}
+	if (n == k) {
+		cout << 0 << endl;
+		return 0;
+	}
+	queue<int>q;
+	q.push(n);
+	line[n][0] = 0;
+	int cnt = 1;
+	while (!q.empty()) {
+		int len = q.size();
+		//cout << cnt<<" "<<len << endl;
+		while (len--) {
+			int num = q.front(); q.pop();
+			if (avail(num - 1) && line[num - 1][cnt % 2]==-1) {
+				q.push(num - 1);
+				line[num - 1][cnt % 2] = cnt;
+			}
+			if (avail(num + 1) && line[num + 1][cnt % 2]==-1) {
+				q.push(num + 1);
+				line[num + 1][cnt % 2] = cnt;
+			}
+			if (avail(num *2) && line[num * 2][cnt % 2]==-1) {
+				q.push(num *2);
+				line[num * 2][cnt % 2] = cnt;
 			}
 		}
+		cnt++;
 	}
-	cout << "finish";
+	j = 0;
+	int mmin = 5000000;
+
+	for (; k <=500000;k+=++j) {
+		if (j>=line[k][j%2]&&mmin>j) {
+			mmin = j;
+		}
+	}
+	cout << (mmin == 5000000 ? -1 : mmin);
+
 }
