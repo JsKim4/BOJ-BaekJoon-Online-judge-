8b86e8087cbd7447be9ebc05eb0e654dfa4bb672
kjs <46150254+JsKim4@users.noreply.github.com>
2020-04-16 23:06:10 +0900

    계단 오르기

--- cpp/2000/2500/P2579.cpp
+++ cpp/2000/2500/P2579.cpp
@@ -1,13 +1,26 @@
 #include<iostream>
+#include<algorithm>
 #pragma warning (disable:4996)
 using namespace std;
 // 2579 계단 오르기
 int n;
 int arr[310];
-int v[310][2];
-int dp() {
+int answer[310][2];
+void dp(int i) {
+	if (i >= n) {
+		return;
+	}
+	answer[i][0] = max(answer[i - 2][0], answer[i - 2][1])+arr[i];
+	answer[i][1] = answer[i - 1][0] + arr[i];
+	dp(i + 1);
 }
 int main(){
 	cin >> n;
-	for (int i = 1; i <= n; i++) cin >> arr[i];
+	for (int i = 0; i < n;i++) 
+		cin >> arr[i];
+	answer[0][0] = arr[0];
+	answer[1][0] = arr[1];
+	answer[1][1] = arr[1] + arr[0];
+	dp(2);
+	cout << max(answer[n - 1][0], answer[n - 1][1]);
 }
